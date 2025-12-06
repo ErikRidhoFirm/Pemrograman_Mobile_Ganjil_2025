82986ce17209e272a4a834daf9d1d4f4ef807a71
ErikRidhoFirm <erikridho1@gmail.com>
2025-12-07 02:15:28 +0700

    Mengupload gambar dan video (error)

---
+++ Pertemuan-10/mydj/lib/components/media_selector.dart
@@ -0,0 +1,157 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:mydj/components/video_preview.dart';
+
+// Tipe data enum untuk menentukan pilihan apakah foto atau video.
+enum MediaType { 
+  photo, 
+  video 
+  }
+
+// Class yang nantinya dipanggil dari halaman lain yang membutuhkan komponen pilih media.
+class MediaSelector extends StatefulWidget 
+{
+  final MediaType mediaType;
+  const MediaSelector({super.key, this.mediaType = MediaType.photo});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MediaSelectorState();
+  }
+}
+
+// Class state seperti biasa..
+class _MediaSelectorState extends State<MediaSelector> {
+  String _mediaPath = ''; // <-- untuk menyimpan path ke media yang dipilih pengguna
+
+  Future<XFile?> _retrieveMedia(ImageSource source) async {
+    final picker = ImagePicker();
+    XFile? media;
+
+    if (widget.mediaType == MediaType.photo) {
+      media = await picker.pickImage(source: source);
+    } else {
+      media = await picker.pickVideo(source: source);
+    }
+    return media;
+  }
+
+  // Memilih media dari kamera.
+  void _selectFromCamera() async {
+    XFile? media = await _retrieveMedia(ImageSource.camera);
+    if (media != null) {
+      setState(() {
+        _mediaPath = media.path;
+      });
+    }
+  }
+
+  // Memilih media dari galeri.
+  void _selectFromGallery() async {
+    XFile? media = await _retrieveMedia(ImageSource.gallery);
+    if (media != null) {
+      setState(() {
+        _mediaPath = media.path;
+      });
+    }
+  }
+
+  // Menghilangkan tampilan media yang dipilh.
+  void _deleteSelected() async {
+    setState(() {
+      _mediaPath = '';
+    });
+  }
+
+  Widget _mediaPlaceholder() {
+    Icon icon;
+    String caption = '';
+    Widget placeholderWidget;
+    //Cek jenis medianya apakah foto atau video
+    if (widget.mediaType == MediaType.photo) {
+      icon = Icon(Icons.image_not_supported, color: Colors.grey);
+      caption = 'No Photo';
+      placeholderWidget = Image.file(
+        File(_mediaPath),
+        width: double.infinity, // <-- isi penuh parent
+        fit: BoxFit.fitWidth, // <-- Tinggi otomatis sesuai rasio
+      );
+    } 
+    else {
+      icon = Icon(Icons.videocam_off, color: Colors.grey);
+      caption = 'No Video';
+      placeholderWidget = VideoPreview(path: _mediaPath); // <-- Nanti kita ganti dengan komponen yang kita buat
+    }
+    //jika path kosong, tampilkan "No photo/Video".
+    if (_mediaPath.isEmpty) {
+      return SizedBox(
+        height: 56, // <-- Tinggi default TextField
+        child: Row(children: [icon, SizedBox(width: 8), Text(caption)]),
+      );
+    }
+    // Jika path ada isinya, tampilkan medianya
+    else {
+      return SizedBox(width: double.infinity, child: placeholderWidget);
+    }
+  }
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child:
+                    _mediaPlaceholder(), // <-- Fungsi yang kita buat tadi dipanggil disini
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                color: Colors.blue[50],
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: FilledButton(
+                        child: Icon(Icons.photo_camera),
+                        onPressed: () {
+                          _selectFromCamera();
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Expanded(
+                      child: FilledButton(
+                        child: Icon(Icons.photo_library),
+                        onPressed: () {
+                          _selectFromGallery();
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Expanded(
+                      child: FilledButton(
+                        child: Icon(Icons.delete),
+                        onPressed: () {
+                          _deleteSelected();
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
